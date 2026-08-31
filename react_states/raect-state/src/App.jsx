af048806cfb1efe31f38cc926e43ca7b08eaf0a6
Sagar Hc <ssag07906@gmail.com>
2026-08-31 22:33:07 +0530

    created our own counter function

---
+++ react_states/raect-state/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./counter"
+
+function App() {
+
+  return (
+    <>
+     <h1>faaaaah</h1>
+     <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ react_states/raect-state/src/counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Counter(){
+    let [count, setCount] =useState(0);
+
+    function incCount(){
+        setCount(count+1);
+        console.log(count);
+    }
+    
+    return(<div>
+        <h1>Count={count} </h1>
+        <button onClick={incCount}>inc</button>
+    </div>
+    
+);
+        
+}
