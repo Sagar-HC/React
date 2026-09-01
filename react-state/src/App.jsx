1ec778ef4b573046a1a01add74fd1c9476656d80
Sagar Hc <ssag07906@gmail.com>
2026-09-01 20:56:43 +0530

    restructuring

---
+++ react-state/src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from "./counter"
+import Like from "../like"
+
+function App() {
+
+  return (
+    <>
+    <h2>states in react </h2>
+    <Counter/>
+     <Like />
+
+    </>
+  )
+}
+
+export default App
