04b2975b7455e1f8514cc6f22ddef782cda0bc6d
Sagar Hc <ssag07906@gmail.com>
2026-09-03 10:39:27 +0530

    learnt how to make todoapp logic

--- ludo/src/todolist.jsx
+++ ludo/src/todolist.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+
 export default function TodoList(){
+    let [todos, setTodos] =useState(["simple task"]);
+    let [newTodos , setNewTodos]= useState([""]);
+
+    let addNewTask = ()=>{
+        setTodos([...todos, newTodos]);
+        //resets the thing to empty.
+        setNewTodos([""]);
+    }
+
+    let updateTodoValue = (event) =>{
+        setNewTodos(event.target.value);
+    }
     return (
-        <h1>faaaaaaaah</h1>
+    <div>
+         <h1>faaaaaaaah</h1>
+         
+         <input placeholder="enter" value = {newTodos} onChange = {updateTodoValue}/> 
+         <button onClick={addNewTask}>submit</button>
+         <br /> 
+         <h1>TodoList</h1> 
+         <ul>{
+                todos.map((todo) =>(
+                        <li>{todo}</li>
+                    ))
+            }</ul>
+
+    </div>
+       
+        
 
         
     )
