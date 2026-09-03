3660de71b39d0b53dca2d79c173801648a33c32c
Sagar Hc <ssag07906@gmail.com>
2026-09-03 11:10:19 +0530

    added key values for individual list items

--- ludo/src/todolist.jsx
+++ ludo/src/todolist.jsx
@@ -1,29 +1,29 @@
 import { useState } from "react";
-
+import { v4 as uuidv4 } from 'uuid';
 export default function TodoList(){
-    let [todos, setTodos] =useState(["simple task"]);
+    let [todos, setTodos] =useState([{task:"sample-task",id: uuidv4()}]);
     let [newTodos , setNewTodos]= useState([""]);
 
     let addNewTask = ()=>{
-        setTodos([...todos, newTodos]);
+        setTodos([...todos, {task:newTodos,id:uuidv4()}]);
         //resets the thing to empty.
         setNewTodos([""]);
     }
 
-    let updateTodoValue = (event) =>{
+    let updateNewTodoValue = (event) =>{
         setNewTodos(event.target.value);
     }
     return (
     <div>
          <h1>faaaaaaaah</h1>
          
-         <input placeholder="enter" value = {newTodos} onChange = {updateTodoValue}/> 
+         <input placeholder="enter" value = {newTodos} onChange = {updateNewTodoValue}/> 
          <button onClick={addNewTask}>submit</button>
          <br /> 
          <h1>TodoList</h1> 
          <ul>{
                 todos.map((todo) =>(
-                        <li>{todo}</li>
+                        <li key = {todo.id}>{todo.task}</li>
                     ))
             }</ul>
 
